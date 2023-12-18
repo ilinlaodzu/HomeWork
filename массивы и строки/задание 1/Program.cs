b038ebae44d8087768404b1fb4d7a5abb955436d
ilinlaodzu <ilinlaodzu2@gmail.com>
2023-12-18 19:40:44 +0300

    массивы и строки

---
+++ массивы и строки/задание 1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задайте двумерный массив символов (тип char [,]).
+// Создать строку из символов этого массива.
+char [] chars = {'a','b','c','d'};
+string str = " ";
+for ( int i=0; i<chars.Length; i++)
+{
+    str += chars [i];
+}
+System.Console.WriteLine (str);
+
+
+//string str = new string (chars);
